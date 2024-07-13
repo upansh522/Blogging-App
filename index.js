730f2ee1251a52d70a7600c85c3eba0fdf182824
[Nishant Upadhyay] <nishantupadhyay813@gmail.com>
2024-07-14 01:52:30 +0530

    ALL SeT

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const mongoDbConnect=require("./connection");
+const staticRoute=require('./routes/staticRoute');
 const path=require("path");
-
-
-app.set('view engine', 'ejs');
-app.use("views",path.resolve("./views"));
-
-mongoDbConnect(" mongodb://127.0.0.1:27017/BlogApp");
-
-const app=express();
+const app = express();
 const PORT= 8000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
+app.set("views", path.resolve("./views")); 
+
+mongoDbConnect("mongodb://127.0.0.1:27017/BlogApp").then(()=>{
+    console.log("mongoDbConnected");
+}).catch((err)=>{
+    console.log(err);
+});
+
+app.use('/haveblog', staticRoute);
 
 app.listen(PORT,()=>{console.log("Server get started !! at port: ",PORT)});
